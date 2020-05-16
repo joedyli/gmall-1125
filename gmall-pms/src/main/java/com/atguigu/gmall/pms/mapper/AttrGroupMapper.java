22b10da46af05133db27b62342ac79fae659e77b
joedy23 <joedy23@aliyun.com>
2020-05-16 16:32:35 +0800

    后台环境搭建完成

---
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/mapper/AttrGroupMapper.java
@@ -0,0 +1,17 @@
+package com.atguigu.gmall.pms.mapper;
+
+import com.atguigu.gmall.pms.entity.AttrGroupEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 属性分组
+ * 
+ * @author fengge
+ * @email fengge@atguigu.com
+ * @date 2020-05-16 14:35:19
+ */
+@Mapper
+public interface AttrGroupMapper extends BaseMapper<AttrGroupEntity> {
+	
+}
