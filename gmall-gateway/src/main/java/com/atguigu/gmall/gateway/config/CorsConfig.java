831847bcf92830635d0c14c31739a6710cfd9d84
joedy23 <joedy23@aliyun.com>
2020-05-18 16:47:27 +0800

    完成规格参数的增删改查

---
+++ gmall-gateway/src/main/java/com/atguigu/gmall/gateway/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package com.atguigu.gmall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+
+        CorsConfiguration config = new CorsConfiguration();
+        // 允许跨域访问的域名。*：代表所有域名跨域跨域访问；不推荐使用* ，使用*无法携带cookie
+        config.addAllowedOrigin("http://manager.gmall.com");
+        // 允许携带cookie信息
+        config.setAllowCredentials(true);
+        // 允许所有类型的请求方法跨域访问
+        config.addAllowedMethod("*");
+        // 允许携带所有的头信息跨域访问
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource configurationSource = new UrlBasedCorsConfigurationSource();
+        // 拦截进入网关服务的所有请求
+        configurationSource.registerCorsConfiguration("/**", config);
+
+        // 初始化cor过滤器
+        return new CorsWebFilter(configurationSource);
+    }
+}
--- gmall-pms/src/main/java/com/atguigu/gmall/pms/controller/AttrGroupController.java
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/controller/AttrGroupController.java
@@ -2,6 +2,7 @@ package com.atguigu.gmall.pms.controller;
 
 import java.util.List;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +34,13 @@ public class AttrGroupController {
 
     @Autowired
     private AttrGroupService attrGroupService;
+
+    @GetMapping("category/{cid}")
+    public ResponseVo<List<AttrGroupEntity>> queryGroupsByCid(@PathVariable("cid")Long cid){
+        List<AttrGroupEntity> attrGroupEntities = this.attrGroupService.list(new QueryWrapper<AttrGroupEntity>().eq("category_id", cid));
+
+        return ResponseVo.ok(attrGroupEntities);
+    }
 
     /**
      * 列表
